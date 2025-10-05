e468eef9bff641d2c7acca66e3f0c26cfc11657a
Zaidzhk10 <zaidzhk10@gmail.com>
2025-10-05 12:44:06 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+print("add =", sum_result)
+print("sub =", diff_result)
